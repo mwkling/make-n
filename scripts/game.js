3229e83e41201d2b7d16b9ef20cb72274cfbfc83
Michael Kling <mike@truejob.com>
2016-12-30 15:09:05 -0500

    add jquery. three boxes draggable with good snapping.

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,8 @@
+'use strict';
+
+var HALF_BOX = 25;
+var positions = {}
+
 function dragMoveListener (event) {
   var target = event.target,
       x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -7,30 +12,53 @@ function dragMoveListener (event) {
       'translate(' + x + 'px, ' + y + 'px)';
   target.setAttribute('data-x', x);
   target.setAttribute('data-y', y);
+
+  positions[target.id] = $(target).position();
 }
 
 function listener (event) {
     console.log(event.type, event.pageX, event.pageY);
 }
 
-interact('.dragnum')
-  .draggable({
-    inertia: true,
-    restrict: {
-      restriction: "parent",
-      endOnly: "true",
-      elementRect: {top: 0, left: 0, bottom: 1, right: 1}
-    },
-    autoScroll: true,
-    onmove: dragMoveListener,
-    snap: {
-      targets: [
-        { x: 200, y: 200, range: 20 }
-      ],
-      relativePoints: [
-        { x: 0.5, y: 0.5 }
-      ]
-    }
+var numSquares = ['box1', 'box2', 'box3']
+
+for (var i = 0; i < numSquares.length; i++) {
+  var id = numSquares[i];
+
+  var notMySquare = numSquares.filter(function(oid) {
+    return id != oid;
   });
+
+  var targets = notMySquare.map(function(id){
+    return [function(x, y) {
+      if (positions[id]) {
+        return { x: positions[id].left - HALF_BOX, y: positions[id].top + HALF_BOX, range: 30 }
+      }
+    },
+    function(x, y) {
+          if (positions[id]) {
+            return { x: positions[id].left + (HALF_BOX * 3), y: positions[id].top + HALF_BOX, range: 30 }
+          }
+        }];
+  });
+
+  targets = targets.reduce(function(a, b) { return a.concat(b); }, []);
+
+  interact("#" + id)
+    .draggable({
+      inertia: true,
+      restrict: {
+        restriction: "parent",
+        endOnly: "true",
+        elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+      },
+      autoScroll: true,
+      onmove: dragMoveListener,
+      snap: {
+        targets: targets,
+        relativePoints: [ { x: 0.5, y: 0.5 } ]
+      }
+    });
+}
 
 window.dragMoveListener = dragMoveListener;
